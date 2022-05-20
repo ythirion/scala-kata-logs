7f6e4907b640e5c98da0da819d23782aa721d160
Yoan Thirion <yoan.thirion@gmail.com>
2022-05-20 09:18:59 +0200

    Remove useless files

--- BankingKata/src/test/scala/EmptyTest.scala
+++
@@ -1,8 +0,0 @@
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers
-
-class EmptyTest extends AnyFunSuite with Matchers{
-  test("scalatest should work") {
-    42 shouldBe 43
-  }
-}
