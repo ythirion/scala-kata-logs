4d6366d67ffa935b57cebdd5a09e4f508c7ba004
Yoan Thirion <yoan.thirion@gmail.com>
2022-11-18 11:14:37 +0100

    go back to starting point

--- GildedRoseKata/exercise/build.sbt
+++ GildedRoseKata/exercise/build.sbt
@@ -5,6 +5,5 @@ version := "1.0"
 scalaVersion := "2.13.8"
 
 libraryDependencies ++= Seq(
-  "com.approvaltests" % "approvaltests" % "18.5.0" % "test",
   "org.scalatest" %% "scalatest" % "3.2.14" % "test"
 )
--- GildedRoseKata/exercise/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ GildedRoseKata/exercise/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -1,18 +1,8 @@
 package com.gildedrose
 
-import org.approvaltests.Approvals
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class GildedRoseTest extends AnyFlatSpec with Matchers {
-  it should "update quality of a common item with approval" in {
-    val items = Array[Item] {
-      new Item("a common item", 0, 0)
-    }
-    val gildedRose = new GildedRose(items)
-
-    gildedRose.updateQuality()
-
-    Approvals.verify(gildedRose.items(0))
-  }
+  it should "update quality of a common item" in {}
 }
